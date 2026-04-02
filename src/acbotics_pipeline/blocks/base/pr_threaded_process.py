6e18615b5d795158eca1b99ea8ec5ae7ebf1b46a
Sam Fladung <sfladung@acbotics.com>
2026-04-02 18:36:11 -0400

    added in timer callback from helping with tracker

--- src/acbotics_pipeline/blocks/base/pr_threaded_process.py
+++ src/acbotics_pipeline/blocks/base/pr_threaded_process.py
@@ -8,13 +8,17 @@ import pyprctl
 
 
 class PR_Threaded_Process(ABC):
-    def __init__(self, max_qsize=1000):
+    def __init__(self, max_qsize=1000, timer_sec=None):
         self.callbacks = []
         self.dataframes = queue.Queue()
         self.thread = threading.Thread(target=self.run_thread)
         self.stop = False
         self.waiting = True
         self.max_qsize = max_qsize
+        self.timer_sec = timer_sec
+        self.last_time = (
+            time.time()
+        )  # should we make this a different time to handle accelerated sim?
 
         self.start()  # TODO. This may want to be moveed out from here to separate start from block creation.
 
@@ -39,16 +43,26 @@ class PR_Threaded_Process(ABC):
         while True:
             if self.stop:
                 break
+            q_timeout = 2
+            if self.timer_sec is not None:
+                dt = time.time() - self.last_time
+                if dt < q_timeout:
+                    q_timeout = dt
             try:
-                data_to_process = self.dataframes.get(timeout=2)
-            except (
-                queue.Empty
-            ):  # Timeout and empty added so it can check for stop without receiving data
-                continue
-            st = time.process_time()
-
-            self.handle_data(data_to_process)
+                data_to_process = self.dataframes.get(timeout=q_timeout)
+                self.handle_data(data_to_process)
+            except queue.Empty:
+                pass
+            if (
+                self.timer_sec is not None
+                and time.time() - self.last_time >= self.timer_sec
+            ):
+                self.timer_callback()
+                self.last_time = time.time()
             self.waiting = self.dataframes.empty()
+
+    def timer_callback(self):
+        pass  # overload if block needs to use a timer callback
 
     def is_waiting(self):
         # verify this is actually thread safe
